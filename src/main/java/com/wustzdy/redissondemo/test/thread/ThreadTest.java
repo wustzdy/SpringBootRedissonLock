fd1f9672ef5b5207589a22664459ee37c2ecfaaf
zhudayang <zhudayang@sensetime.com>
2021-03-31 19:36:35 +0800

    分布式锁

--- src/main/java/com/wustzdy/redissondemo/test/thread/ThreadTest.java
+++ src/main/java/com/wustzdy/redissondemo/test/thread/ThreadTest.java
@@ -2,8 +2,14 @@ package com.wustzdy.redissondemo.test.thread;
 
 public class ThreadTest {
     public static void main(String[] args) {
-        Thread thread = new Thread(new ThreadDemo());
-        thread.start();
+        Thread thread1 = new Thread(new ThreadDemo());
+        Thread thread2 = new Thread(new ThreadDemo());
+        Thread thread3 = new Thread(new ThreadDemo());
+        Thread thread4 = new Thread(new ThreadDemo());
+        thread1.start();
+        thread2.start();
+        thread3.start();
+        thread4.start();
     }
 
     private static class ThreadDemo implements Runnable {
